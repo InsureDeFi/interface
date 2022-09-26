2d1cfa98b7bfc9e19defa804419ef6c7286f4a16
chaitanyasjoshi <chaitanya9joshi@gmail.com>
2022-09-26 14:17:10 +0530

    pref: dont rely on subgraph

--- src/hooks/useLiquidityData.tsx
+++ src/hooks/useLiquidityData.tsx
@@ -1,26 +1,17 @@
 import { AddressZero } from '@ethersproject/constants';
-import { useAccount, useContractRead, useNetwork } from 'wagmi';
+import { useAccount, useBalance, useContractRead, useNetwork } from 'wagmi';
 import { useMemo } from 'react';
-import { useLiquidityPositionQuery, useSnapshotsQuery, useEventsQuery } from './graphql/user-data-provider/hooks';
+import { useSnapshotsQuery, useEventsQuery } from './graphql/user-data-provider/hooks';
 import { getReturns } from '@utils/returns';
 import { usePoolDetails } from './usePoolDetails';
 import { getPoolAddress } from '@utils/networksConfig';
-import { formatBignumber, stringToBignumber } from '@utils/helper';
+import { formatBignumber } from '@utils/helper';
 import riskPoolAbi from 'assets/abi/RiskPool.json';
 
 export function useLiquidityData() {
   const { address } = useAccount();
   const { totalLiquidity, sharesTotalSupply } = usePoolDetails();
   const { chain } = useNetwork();
-
-  const { data: position } = useLiquidityPositionQuery({
-    variables: {
-      account: address?.toLowerCase() || AddressZero,
-    },
-    pollInterval: 4000,
-  });
-
-  const shares = position?.liquidityPosition?.shares;
 
   //TODO
   const { data: snapshots } = useSnapshotsQuery({
@@ -35,11 +26,18 @@ export function useLiquidityData() {
     pollInterval: 4000,
   });
 
+  const { data: shares } = useBalance({
+    addressOrName: address,
+    token: getPoolAddress(chain),
+    enabled: !!address,
+    watch: true,
+  });
+
   const { data: assets } = useContractRead({
     addressOrName: getPoolAddress(chain),
     contractInterface: riskPoolAbi,
     functionName: 'convertToAssets',
-    args: stringToBignumber(shares, 6),
+    args: shares?.value,
     enabled: !!shares,
     watch: true,
   });
@@ -48,7 +46,7 @@ export function useLiquidityData() {
     if (shares && snapshots && events && address) {
       const returnData = getReturns(snapshots, events, sharesTotalSupply, totalLiquidity);
       return {
-        shares: shares,
+        shares: shares.formatted,
         assets: formatBignumber(assets, 6),
         principal: returnData.principal,
         fees: returnData.fees.toString(),
