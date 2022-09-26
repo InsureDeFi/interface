8e8bb04c5067e54891c2f8aa549962de6304d73d
chaitanyasjoshi <chaitanya9joshi@gmail.com>
2022-09-26 14:24:48 +0530

    fix: display polling of active chain

--- src/components/Polling/Polling.tsx
+++ src/components/Polling/Polling.tsx
@@ -1,9 +1,13 @@
+import { getDefaultChainId } from '@utils/networksConfig';
 import React from 'react';
 import { Circle } from 'react-feather';
-import { useBlockNumber } from 'wagmi';
+import { useBlockNumber, useNetwork } from 'wagmi';
 
 export default function Polling() {
-  const { data: latestBlock, isError, isIdle } = useBlockNumber({ watch: true });
+  const { chain } = useNetwork();
+  const defaultChainId = getDefaultChainId();
+  const activeChainId = chain?.id || defaultChainId;
+  const { data: latestBlock, isError, isIdle } = useBlockNumber({ chainId: activeChainId, watch: true });
   return (
     <div className="fixed bottom-4 right-4 hidden select-none items-center justify-center gap-1 md:flex">
       <div
