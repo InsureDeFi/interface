f22f23670e5603ab2a378e3d2479b97387742d51
chaitanyasjoshi <chaitanya9joshi@gmail.com>
2022-10-06 10:47:27 +0530

    style: improve readability

--- src/components/Polling/Polling.tsx
+++ src/components/Polling/Polling.tsx
@@ -1,21 +1,17 @@
 import { getDefaultChainId } from '@utils/networksConfig';
 import React from 'react';
-import { Circle } from 'react-feather';
+import { Box } from 'react-feather';
 import { useBlockNumber, useNetwork } from 'wagmi';
 
 export default function Polling() {
   const { chain } = useNetwork();
   const defaultChainId = getDefaultChainId();
-  const activeChainId = chain?.id || defaultChainId;
+  const activeChainId = (chain?.unsupported ? defaultChainId : chain?.id) || defaultChainId;
   const { data: latestBlock, isError, isIdle } = useBlockNumber({ chainId: activeChainId, watch: true });
   return (
-    <div className="fixed bottom-4 right-4 hidden select-none items-center justify-center gap-1 md:flex">
-      <div
-        className={`text-xs font-medium ${isIdle ? 'text-yellow-500' : isError ? 'text-red-500' : 'text-green-500'}`}
-      >
-        {latestBlock}
-      </div>
-      <Circle size={8} fill={`${isIdle ? '#EAB308' : isError ? '#EF4444' : '#23C45E'}`} strokeWidth={0} />
+    <div className="fixed bottom-4 right-4 hidden select-none items-center justify-center gap-1.5 md:flex">
+      <div className={`text-sm text-zinc-200`}>{latestBlock}</div>
+      <Box size={14} color={`${isIdle ? '#EAB308' : isError ? '#EF4444' : '#23C45E'}`} strokeWidth={3} />
     </div>
   );
 }
